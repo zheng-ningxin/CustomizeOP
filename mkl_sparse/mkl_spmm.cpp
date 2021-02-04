48d1636550323b8856c40c63928ac6990d882b1c
Ningxin <Ningxin.Zheng@microsoft.com>
2021-02-04 06:28:15 +0000

    Fix the bug caused by transpose.
    We need call contiguouse befer we call the data_ptr function.

--- mkl_sparse/mkl_spmm.cpp
+++ mkl_sparse/mkl_spmm.cpp
@@ -6,6 +6,16 @@
 #include "mkl_spblas.h"
 #include "mkl_types.h"
 
+template<typename scalar_t>
+void debug_show(int * row_index, int n_row, int * col_index, scalar_t * value){
+    std::cout<<"Debug Info"<<std::endl;
+    int length = row_index[n_row];
+    for(int i=0;i<length;i++){
+        std::cout<< i <<" "<<value[i]<<std::endl;
+    }
+
+}
+
 template <typename scalar_t>
 sparse_status_t mkl_sparse_mm(
     int M, int K, int N,
@@ -88,9 +98,10 @@ int mkl_spmm(
     scalar_t alpha,
     scalar_t beta)
 {
+
+    // debug_show<scalar_t>(row_index, M, col_index, values);
     sparse_matrix_t SA;
     sparse_status_t status;
-
 
     status = mkl_create_csr<scalar_t>(&SA, M, K, row_index, &(row_index[1]), col_index, values);
     if (status != SPARSE_STATUS_SUCCESS)
@@ -99,11 +110,10 @@ int mkl_spmm(
         return -2;
     }
 
-
     matrix_descr descr;
     descr.type = SPARSE_MATRIX_TYPE_GENERAL;
-    descr.mode = SPARSE_FILL_MODE_LOWER;
-    descr.diag = SPARSE_DIAG_NON_UNIT;
+    // descr.mode = SPARSE_FILL_MODE_LOWER;
+    // descr.diag = SPARSE_DIAG_NON_UNIT;
     status = mkl_sparse_set_mm_hint(SA, SPARSE_OPERATION_NON_TRANSPOSE, descr, SPARSE_LAYOUT_ROW_MAJOR, K, 1);
 
     if (status != SPARSE_STATUS_SUCCESS)
@@ -133,9 +143,12 @@ at::Tensor mkl_sparse_linear_forward(
     int in_features = input.size(1);
     int batchsize = input.size(0);
     // MKL sparse only support: sparse matrix * dense matrix
-    torch::Tensor t_input = input.t();
-    torch::Tensor t_output = torch::zeros({out_features, batchsize});
+    torch::Tensor t_input = input.t().contiguous() ;
+    /// Note transpose just change the view of the tensor, the data
+    /// that in the data_ptr are still not transposed! So we need call
+    /// contiguous to make the data layout correct
 
+    torch::Tensor t_output = torch::zeros({out_features, batchsize});
     AT_DISPATCH_FLOATING_TYPES(input.type(), "mkl_sparse_linear_forward", ([&] {
                                    mkl_spmm(
                                        out_features,
@@ -147,7 +160,7 @@ at::Tensor mkl_sparse_linear_forward(
                                        t_input.data_ptr<scalar_t>(),
                                        t_output.data_ptr<scalar_t>(),
                                        static_cast<scalar_t>(1),
-                                       static_cast<scalar_t>(1));
+                                       static_cast<scalar_t>(0));
                                }));
     // need transpose again
     return t_output.t() + bias;
